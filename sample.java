1f3fd1a128942d3f540c102121c9839247e8e99b
root <root@ip-172-31-28-240.ap-southeast-2.compute.internal>
2026-05-18 10:22:37 +0000

    LOGIN PAGE

---
+++ sample.java
@@ -0,0 +1,7 @@
+// Save this file as HelloWorld.java
+public class HelloWorld {
+    // The main method is the starting point of any Java application
+    public static void main(String[] args) {
+        // This prints "Hello, World!" to the console
+        System.out.println("Hello, World!");}
+
